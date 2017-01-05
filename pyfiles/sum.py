72893a4465b1a0973117c62d91623474ac7070f8
manojdevs <manojselvamdev@gmail.com>
2017-01-05 16:40:24 +0530

    modified sum.py

--- pyfiles/sum.py
+++ pyfiles/sum.py
@@ -2,7 +2,7 @@ import redis
 chk=1
 cache=redis.StrictRedis(host='redis',port=6379,db=0)
 while chk!=0:
-	opt=int(raw_input("select \n1)save \n2)load \n0)Exit"))
+	opt=int(raw_input("select \n1)create \n2)load \n0)Exit"))
 	if(opt==1):
 		id=raw_input("Enter id:")
 		name=raw_input("Enter name:")
